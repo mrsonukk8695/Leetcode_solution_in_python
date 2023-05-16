a179bd4a9e5ef4d30ac3095f577f84d91bfc882b
Sonu Kumar Kushwaha <105097393+mrsonukk8695@users.noreply.github.com>
2023-05-16 23:04:19 +0530

    Update 1-two-sum.py

--- 0001-two-sum/1-two-sum.py
+++ 0001-two-sum/1-two-sum.py
@@ -1,12 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        o=[]
-        for j in range(len(nums)-1):
-            for i in range(j+1,len(nums)):
-                 if (nums[j]+nums[i])==target:
-                        o.append(j)
-                        o.append(i)
-        return o
-                        
-                
-        
+        i= 0
+        while True:
+            for j in range(i+1,len(nums)):
+                if nums[i] == target - nums[j]:
+                    return [i,j]       
+            i+=1
